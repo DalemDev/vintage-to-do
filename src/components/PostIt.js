70145485b5496b5fa6cde640278f305f25b08dd5
dalemdev <developerj659@gmail.com>
2023-04-09 17:28:04 -0500

    solucion problema notas

--- src/components/PostIt.js
+++ src/components/PostIt.js
@@ -1,37 +1,34 @@
+import { useState } from "react";
 import "../css/post-it.css";
 import { AiFillCloseCircle } from "react-icons/ai";
 
 function PostIt({ titulo, texto, idPostIt }) {
+  const [modalAbierta, setModalAbierta] = useState(false);
 
-  const abrirPostIt = (e) => {  
-    const modales = document.querySelectorAll(".modal-postit");
-    modales.forEach(modal => {
-      if(modal.id === e.target.id){
-        modal.style.transform = "scale(1)";
-      }
-    })
-    
+  const abrirPostIt = (e) => {
+    setModalAbierta(true);
   };
 
-  const cerrarPostIt = (e) => {
-    const modales = document.querySelectorAll(".modal-postit");
-    modales.forEach(modal => {
-      modal.style.transform = "scale(0)";      
-    })
+  const cerrarPostIt = () => {
+    setModalAbierta(false);
   };
+
+  const modalClass = modalAbierta ? "modal-postit open" : "modal-postit";
 
   return (
-      <li className="post-li">
-        <a href="#!" className="post-link" onClick={abrirPostIt}>
-          <h2 className="post-titulo" id={idPostIt}>{titulo}</h2>
-        </a>
-        <div className="modal-postit" id={idPostIt}>
-          <button className="modal-cerrar" onClick={cerrarPostIt}>
-            <AiFillCloseCircle />
-          </button>
-          <p className="post-texto">{texto}</p>
-        </div>
-      </li>
+    <li className="post-li">
+      <button className="post-link" onClick={abrirPostIt}>
+        <h2 className="post-titulo" id={idPostIt}>
+          {titulo}
+        </h2>
+      </button>
+      <div className={modalClass} id={idPostIt}>
+        <button className="modal-cerrar" onClick={cerrarPostIt}>
+          <AiFillCloseCircle />
+        </button>
+        <p className="post-texto">{texto}</p>
+      </div>
+    </li>
   );
 }
 
